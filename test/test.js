f55a32a31d1d9aa1833ea87080cb92f27f919b8b
hrgdavor <hrgdavor@gmail.com>
2017-12-24 11:09:38 +0100

    fix confilct

--- test/test.js
+++ test/test.js
@@ -1,24 +1,5 @@
 
-
-<<<<<<< HEAD
-describe('babel-plugin-translate-mi2', () => {
-=======
 describe('babel-plugin-jsx-mi2', () => {
-  it('should contain text', () => {
-    const vnode = render(h => <div>test</div>)
-    expect(vnode.tag).toEqual('div')
-    expect(vnode.children[0]).toEqual('test')
-  })
-
-  it('should spread attribs', () => {
-    const sth = {name:'Joe'};
-    const vnode = render(h => <div {...sth}>test</div>)
-    
-    expect(vnode.tag).toEqual('div')
-    expect(vnode.children[0]).toEqual('test')
-    expect(vnode.attr.name).toEqual('Joe')
-  })
->>>>>>> ff0f54ae1b37d30143549a8a011b8da3572814b1
 
   // translation implementation
   var TRANS = {name:'Name', city:'City'};
